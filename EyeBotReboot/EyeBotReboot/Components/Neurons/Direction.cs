b46710d82bd8e2f2495f13356b8ecd939e4165b5
Patrick Abbs <patricka@headspring.com>
2013-09-23 23:33:43 -0500

    direction neuron input added

--- EyeBotReboot/EyeBotReboot/Components/Neurons/Direction.cs
+++ EyeBotReboot/EyeBotReboot/Components/Neurons/Direction.cs
@@ -7,6 +7,21 @@ namespace EyeBotReboot.Components.Neurons
 {
     public class Direction: INeuron
     {
+                public Direction(float direction,
+                              float otherDirectionThresholdBase, float otherDirectionThresholdSpike,
+                              float otherDirectionThresholdDecayPercent, float otherDirectionThresholdDecayConstant,
+                              float otherDirectionSignalStrength,
+
+                              float representativeThresholdBase, float representativeDirectionThresholdSpike,
+                              float representativeDirectionThresholdDecayPercent,
+                              float representativeDirectionThresholdDecayConstant,
+                              float representativeDirectionSignalStrength,
+
+                              float laplaceFilterThresholdBase, float laplaceFilterThresholdSpike,
+                              float laplaceFilterThresholdDecayPercent, float laplaceFilterThresholdDecayConstant,
+                              float laplaceFilterSignalStrength)
+        {
+        }
         public float Charge { get; set; }
     }
 }
